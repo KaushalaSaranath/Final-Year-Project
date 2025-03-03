52880d48313b847064f8b4caf8f5412659d22685
KaushalaSaranath <kaushala.saranath7@gmail.com>
2025-03-03 11:26:53 +0530

    change comments

--- zoocura/lib/services/api_service.dart
+++ zoocura/lib/services/api_service.dart
@@ -3,17 +3,19 @@ import 'package:http/http.dart' as http;
 import 'package:path/path.dart';
 import 'package:mime/mime.dart';
 import 'dart:convert';
-import 'package:http_parser/http_parser.dart'; // Import this for MediaType
+import 'package:http_parser/http_parser.dart'; // Import MediaType
 
 class ApiService {
-  static const String _baseUrl = "http://172.27.42.180:3000"; // Replace with actual server IP
+  static const String _baseUrl =
+      "http://192.168.8.105:3000"; //server IP
 
   static Future<String> uploadImage(File imageFile) async {
     try {
       String fileName = basename(imageFile.path);
       String? mimeType = lookupMimeType(imageFile.path);
 
-      var request = http.MultipartRequest("POST", Uri.parse("$_baseUrl/predict"));
+      var request =
+          http.MultipartRequest("POST", Uri.parse("$_baseUrl/predict"));
       request.files.add(await http.MultipartFile.fromPath(
         'file',
         imageFile.path,
